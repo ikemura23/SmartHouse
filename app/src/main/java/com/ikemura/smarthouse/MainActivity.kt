5e593278a16d85dec8f9396c6a018e178772e05c
ikemura23 <banbara.work@gmail.com>
2020-06-30 17:37:39 +0900

    リファクタリング

--- app/src/main/java/com/ikemura/smarthouse/MainActivity.kt
+++ app/src/main/java/com/ikemura/smarthouse/MainActivity.kt
@@ -10,20 +10,6 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.ikemura.smarthouse.databinding.ActivityMainBinding
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the PeripheralManager
- * For example, the snippet below will open a GPIO pin and set it to HIGH:
- *
- * val manager = PeripheralManager.getInstance()
- * val gpio = manager.openGpio("BCM6").apply {
- *     setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * }
- * gpio.value = true
- *
- * You can find additional examples on GitHub: https://github.com/androidthings
- */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
@@ -50,16 +36,14 @@ class MainActivity : AppCompatActivity() {
         databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val snapshotValue: HashMap<String, String> = snapshot.value as HashMap<String, String>
-                // snapshotValue.forEach { (key, value) ->
-                //     Log.d(TAG, "key:$key, value:$value")
-                // }
                 val energies: List<Energy> = snapshotValue.map { Energy(date = it.key, electric = it.value) }.toList()
                 Log.d(TAG, energies.toString())
                 bindToNowEnergyView(energies.first())
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.e(TAG, error.message)
+                // Log.e(TAG, error.message)
+                error.toException().printStackTrace()
             }
         })
     }
